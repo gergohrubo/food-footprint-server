5a7232f47ab0b9fc96ab5af0f7a81af9c7fc6b7f
gergohrubo <gergohrubo@gmail.com>
2020-01-13 16:00:32 +0100

    getRecipe function added to make a guess on the name of the dish

--- apis/spoonacular.js
+++ apis/spoonacular.js
@@ -19,4 +19,16 @@ async function getNutrition(ingredientsArray) {
   }
 }
 
-module.exports = { getNutrition }
+async function getRecipe(ingredientsArray) {
+  try {
+    const baseUrl = `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${process.env.SPOONACULAR_API_KEY}`
+    const ingredientsQuery = ingredientsArray.reduce((acc, item) => acc += `,+${item}`)
+    const url = baseUrl + '&ingredients=' + ingredientsQuery + '&number=5&ranking=1&ignorePantry=true'
+    const res = await request.get(url)
+    return res
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+module.exports = { getNutrition, getRecipe }
